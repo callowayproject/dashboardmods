47796ca568fee9218c5cfaeef7a16602db9f134d
Corey Oordt <coordt@washingtontimes.com>
2011-05-21 20:42:22 -0400

    [NEW] added some config to the example app for testing

--- example/dashboard.py
+++ example/dashboard.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from admin_tools.dashboard import Dashboard
 from admin_tools.dashboard import modules
 from dashboardmods import get_memcache_dash_modules, get_rss_dash_modules, get_varnish_dash_modules
-
+from dashboardmods.modules import GraphDashboardModule
 
 class CustomIndexDashboard(Dashboard):
     """
@@ -13,6 +13,8 @@ class CustomIndexDashboard(Dashboard):
         Dashboard.__init__(self, **kwargs)
 
         # append a link list module for "quick links"
+        self.children.append(GraphDashboardModule('timeonsite'))
+
         self.children.append(modules.LinkList(
             title=_('Quick links'),
             layout='inline',
@@ -47,19 +49,6 @@ class CustomIndexDashboard(Dashboard):
             include_list=('django.contrib',),
         ))
 
-        # # append a recent actions module
-        # self.children.append(RecentActionsDashboardModule(
-        #     title=_('Recent Actions'),
-        #     limit=5
-        # ))
-        # 
-        # # append a feed module
-        # self.children.append(FeedDashboardModule(
-        #     title=_('Latest Web Development Activities'),
-        #     feed_url='http://www.pivotaltracker.com/user_activities/de98454d195bc113ba489ff01ae2673a',
-        #     limit=5
-        # ))
-        
         self.children.extend(get_memcache_dash_modules())
         self.children.extend(get_varnish_dash_modules())
         self.children.extend(get_rss_dash_modules())
